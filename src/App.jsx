020c4e04c9dd7443ae4669abe32d50a465407cfa
ogozielamgift <ogozielamgift@gmail.com>
2025-11-05 15:52:21 +0100

    src

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>hey</>;
+  return (
+    <>
+      <h2>WELCOME TO OUR COLLAB PROJECT</h2>
+    </>
+  );
 }
 
 export default App;
