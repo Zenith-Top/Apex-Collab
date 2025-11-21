055cd725db6b5ddf02a25cd3ad4b437d3535c0ca
KalmCode96 <ibekwevictor2000@gmail.com>
2025-11-21 15:04:13 +0100

    src

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Nav from "./Components/Nav/Nav";
 import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
+import RoomsRating from "./Components/RoomsRating/RoomsRating";
 
 function App() {
   return (
     <>
       <Nav />
       <Home />
+      <RoomsRating />
       <Footer />
     </>
   );
---
+++ src/Components/RoomsRating/RoomsRating.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const RoomsRating = () => {
+  return (
+    <div>RoomsRating</div>
+  )
+}
+
+export default RoomsRating
